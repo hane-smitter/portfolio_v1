3211d892481d9ada11e16188025e5e8ef63857f5
hane-smitter <hanesmitter3@gmail.com>
2021-07-27 17:14:12 +0300

    social icons ui change

--- public/scripts/typedjs.js
+++ public/scripts/typedjs.js
@@ -1,4 +1,4 @@
 var type = new Typed('.typed', {
-  strings: ['<strong>Developer</strong>^2000', '<strong>Web Developer</strong>', '<strong>Software Engineer</strong>'],
+  strings: ['<strong>Developer</strong>^2000', '<strong>Web Developer</strong>', '<strong>Software Developer</strong>'],
   typeSpeed: 70
 });
